a0f8012fa9799536b3f01aa6af78d1752aab0eb8
beinganidiot <ocaranzacosta@gmail.com>
2023-03-15 23:17:27 +0000

    final home code, some css work

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import clock icon from react-icons library
+import {FaClock} from 'react-icons/fa'
+
+//import SecondsCounter
+import SecondsCounter from "./secondscounter.jsx";
+
+//import styles for counter
+import "../../styles/index.css";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="MainFrame">
+			<span><FaClock /></span>
+			<span>Hello!</span>
 		</div>
-	);
+
+	
+		);
 };
 
 export default Home;
